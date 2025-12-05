292954e11f0f09d41aef03a54ebe3ae939c633b2
FouedbENIDIR <foued.benidir@groupe-esigelec.org>
2025-12-05 17:13:02 +0100

    fix: add GitHub Pages deployment config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // <--- Vérifiez cette ligne
 
 export default defineConfig({
+  base: '/davicen_site/',
   plugins: [
     react(),
     tailwindcss(), // <--- Et cette ligne
